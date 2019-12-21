80405a17640a450730535da50dbcef8dd2b31e98
root <root@localhost.localdomain>
2019-12-21 16:40:40 +0800

    added highscore and end game function + optimizations to code

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class App(tk.Tk):
     def __init__(self, screenName=None, baseName=None, className="Tk", useTk=1, sync=0, use=None):
         super().__init__(screenName=screenName, baseName=baseName, className=className, useTk=useTk, sync=sync, use=use)
         self.geometry("1200x720")
+        self.resizable(0,0)
         self._frame = None
         self.switch(Welcome)
     def switch(self,frame):
@@ -27,12 +28,25 @@ class Welcome(tk.Frame):
 class Main(tk.Frame):
     def __init__(self, master=None,**kw):
         super().__init__(master=master,**kw)
+        try:
+            with open("score.txt") as f:
+                self.highscore = int(f.read())
+        except FileNotFoundError:
+            self.highscore = 0
+        self.score = 0
         game_container = tk.Frame(self,height=720,width=720,bg="#777777")
         self.all_cells = [[Cell(game_container,row=0,column=0,height=180,width=180),Cell(game_container,row=0,column=1,height=180,width=180),Cell(game_container,row=0,column=2,height=180,width=180),Cell(game_container,row=0,column=3,height=180,width=180)],
                           [Cell(game_container,row=1,column=0,height=180,width=180),Cell(game_container,row=1,column=1,height=180,width=180),Cell(game_container,row=1,column=2,height=180,width=180),Cell(game_container,row=1,column=3,height=180,width=180)],
                           [Cell(game_container,row=2,column=0,height=180,width=180),Cell(game_container,row=2,column=1,height=180,width=180),Cell(game_container,row=2,column=2,height=180,width=180),Cell(game_container,row=2,column=3,height=180,width=180)],
                           [Cell(game_container,row=3,column=0,height=180,width=180),Cell(game_container,row=3,column=1,height=180,width=180),Cell(game_container,row=3,column=2,height=180,width=180),Cell(game_container,row=3,column=3,height=180,width=180)]]
-        game_container.pack()
+        game_container.grid(row=0,column=0)
+        score = tk.Frame(self,height=720,width=720)
+        self.points= tk.StringVar()
+        self.over = tk.StringVar()
+        self.points.set(f"Score: 0\nHighscore:{self.highscore}")
+        tk.Label(score,font=("Arial",20),textvariable=self.points,justify=tk.LEFT).pack()
+        tk.Label(score,font=("Arial",20),textvariable=self.over,justify=tk.LEFT).pack()
+        score.grid(row=0,column=1,sticky=tk.N)
         self.bind("<Up>",self.up)
         self.bind("<Down>",self.down)
         self.bind("<Left>",self.left)
@@ -57,7 +71,26 @@ class Main(tk.Frame):
                 break
 
     def check(self):
-        print("gg.com")
+        dead = True
+        for y in self.all_cells:
+            for x in y:
+                if not x.row == 0:
+                    if self.all_cells[x.row-1][x.column].count == x.count:
+                        dead = False
+                if not x.column == 0:
+                    if self.all_cells[x.row][x.column-1].count == x.count:
+                        dead = False
+                if not x.row == 3:
+                    if self.all_cells[x.row + 1][x.column].count == x.count:
+                        dead = False
+                if not x.column == 3:
+                    if self.all_cells[x.row][x.column+1].count == x.count:
+                        dead = False
+                if not dead:
+                    break
+            if not dead:
+                break
+        return dead
     
     def refresh(self):
         while True:
@@ -68,32 +101,46 @@ class Main(tk.Frame):
                 self.all_cells[cell[0]][cell[1]].count = 1
                 self.all_cells[cell[0]][cell[1]].change_image()
                 break
+    
+    def finish(self):
+        string = "Game over!\n"
+        if self.score == self.highscore:
+            string += ("Congratulations you have beaten your high score!")
+            with open("score.txt","w+") as f:
+                f.write(str(self.highscore))
+        self.over.set(string)
+    
+    def add_points(self,count):
+        self.score += 2**count
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.points.set(f"Score:{self.score}\nHighscore:{self.highscore}")
 
-    def merge(self,new,old,direction,x,y,z):
+    def go(self,new,old,direction,x,y,z):
         if direction == "+y":
             old.move(old.object,0,old.y*180)
-            new.count= new.count + 1
+            new.count=old.count
             new.change_image()
             old.count = 0
             old.change_image()
         elif direction == "-y":
             old.move(old.object,0,old.y*-180)
-            new.count= new.count + 1
+            new.count=old.count
             new.change_image()
             old.count = 0
-            old.change_image()    
+            old.change_image() 
         elif direction == "-x":
             old.move(old.object,old.x*-180,0)
-            new.count= new.count + 1
+            new.count=old.count
             new.change_image()
             old.count = 0
-            old.change_image() 
+            old.change_image()
         elif direction == "+x":
             old.move(old.object,old.x*+180,0)
-            new.count= new.count + 1
+            new.count=old.count
             new.change_image()
             old.count = 0
-            old.change_image() 
+            old.change_image()
         old.x = 0
         old.y = 0
         new.x = 0
@@ -114,22 +161,34 @@ class Main(tk.Frame):
                         moved,move = True,True
                     elif target.count == current.count:
                         current.y += 1
-                        self.merge(target,current,"-y",x,y,z)
+                        current.count += 1
+                        self.add_points(current.count)
+                        self.go(target,current,"-y",x,y,z)
                         merged,merge = True,True
                         break
                     else:
                         target = self.all_cells[z+1][x]
                         break
                 if not merged and moved:
-                    current.move(current.object,0,current.y*-180)
-                    target.count=current.count
-                    target.change_image()
-                    current.count = 0
-                    current.change_image()
-                    current.y = 0
-                    target.y = 0
+                    self.go(target,current,"-y",x,y,z)
         if merge or move:
             self.refresh()
+        if not merge and not move:
+            over,alive = False,False
+            for y in self.all_cells:
+                for x in y:
+                    if x.count == 0:
+                        alive = True
+                        break
+                if alive:
+                    break
+            if not alive:
+                over = True
+                    
+            if over:
+                dead = self.check()
+                if dead:
+                    self.finish()
 
     def down(self,event):
         merge,move = False,False
@@ -146,22 +205,34 @@ class Main(tk.Frame):
                         moved,move = True,True
                     elif target.count == current.count:
                         current.y += 1
-                        self.merge(target,current,"+y",x,y,z)
+                        current.count += 1
+                        self.add_points(current.count)
+                        self.go(target,current,"+y",x,y,z)
                         merged,merge = True,True
                         break
                     else:
                         target = self.all_cells[z-1][x]
                         break
                 if not merged and moved:                       
-                    current.move(current.object,0,current.y*180)
-                    target.count=current.count
-                    target.change_image()
-                    current.count = 0
-                    current.change_image()
-                    current.y = 0
-                    target.y = 0
+                    self.go(target,current,"+y",x,y,z)
         if merge or move:
             self.refresh()
+        if not merge and not move:
+            over,alive = False,False
+            for y in self.all_cells:
+                for x in y:
+                    if x.count == 0:
+                        alive = True
+                        break
+                if alive:
+                    break
+            if not alive:
+                over = True
+                    
+            if over:
+                dead = self.check()
+                if dead:
+                    self.finish()
                     
                     
 
@@ -180,22 +251,33 @@ class Main(tk.Frame):
                         moved,move = True,True
                     elif target.count == current.count:
                         current.x += 1
-                        self.merge(target,current,"-x",x,y,z)
+                        current.count += 1
+                        self.add_points(current.count)
+                        self.go(target,current,"-x",x,y,z)
                         merged,merge = True,True
                         break
                     else:
                         target = self.all_cells[y][z+1]
                         break
                 if not merged and moved:
-                    current.move(current.object,current.x*-180,0)
-                    target.count=current.count
-                    target.change_image()
-                    current.count = 0
-                    current.change_image()
-                    target.x = 0
-                    current.x = 0
+                    self.go(target,current,"-x",x,y,z)
         if merge or move:
             self.refresh()
+        if not merge and not move:
+            over,alive = False,False
+            for y in self.all_cells:
+                for x in y:
+                    if x.count == 0:
+                        alive = True
+                        break
+                if alive:
+                    break
+            if not alive:
+                over = True
+            if over:
+                dead = self.check()
+                if dead:
+                    self.finish()
 
     def right(self,event):
         merge,move=False,False
@@ -212,22 +294,33 @@ class Main(tk.Frame):
                         moved,move = True,True
                     elif target.count == current.count:
                         current.x += 1
-                        self.merge(target,current,"+x",x,y,z)
+                        current.count += 1
+                        self.add_points(current.count)
+                        self.go(target,current,"+x",x,y,z)
                         merged,merge = True,True
                         break
                     else:
                         target = self.all_cells[y][z-1]
                         break
                 if not merged and moved:
-                    current.move(current.object,current.x*180,0)
-                    target.count=current.count
-                    target.change_image()
-                    current.count = 0
-                    current.change_image()
-                    current.x = 0
-                    target.x = 0
+                    self.go(target,current,"+x",x,y,z)
         if merge or move:
             self.refresh()
+        if not merge and not move:
+            over,alive = False,False
+            for y in self.all_cells:
+                for x in y:
+                    if x.count == 0:
+                        alive = True
+                        break
+                if alive:
+                    break
+            if not alive:
+                over = True
+            if over:
+                dead = self.check()
+                if dead:
+                    self.finish()
         
 class Cell(tk.Canvas):
     def __init__(self, master=None,row=0,column=0,count=0,**kw):
